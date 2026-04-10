0fa22c9b64239163853ae1ba1fc2d167b4faa405
rasyazahra <rasyazahra@upi.edu>
2026-04-10 23:04:51 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative paths for assets on GH Pages
+  base: '/tugas_pengembanganmedia_individu/', // Jalur mutlak ke repository GitHub Pages Anda
 })
